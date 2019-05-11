7ba930789bf471855179d6f6d73afd68c67f36f9
TheArczi <thearczi@live.com>
2019-05-11 17:24:52 +0200

    Pierwsza wersja gry - ukonczony tutorial

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    const string PICK_UP_TAG = "Pick Up";
+    public float speed;
+    private int pickUpsCount;
+    public Text pointsText;
+    public Text winText;
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        pickUpsCount = 0;
+        foreach (GameObject pickUp in GameObject.FindGameObjectsWithTag(PICK_UP_TAG))
+        {
+            pickUpsCount += 1;
+        }
+        updatePointsText();
+        
+    }
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement * speed);
+        if (pickUpsCount == 0)
+        {
+            winText.gameObject.SetActive(true);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            pickUpsCount -= 1;
+            updatePointsText();
+        }
+    }
+
+    private void updatePointsText()
+    {
+        pointsText.text = pickUpsCount.ToString();
+    }
+}
---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    public float jumpingSpeed;
+    private float startYPosition;
+    private float changerPosition;
+    private float speedAcceleration;
+    private string moveDirectory = "down";
+
+    private void Start()
+    {
+        startYPosition = transform.position.y;
+    }
+    private void Update()
+    {
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+        updateSpeed();
+        changePosition();
+        transform.position = new Vector3(transform.position.x, startYPosition + changerPosition, transform.position.z);
+    }
+
+    private void updateSpeed()
+    {
+        if (changerPosition > 0.30f)
+        {
+            moveDirectory = "down";
+            speedAcceleration = 0.0008f;
+        }
+        else if (changerPosition > 0.25f)
+        {
+            speedAcceleration = 0.0018f;
+        }
+        else if (changerPosition > 0.20f)
+        {
+            speedAcceleration = 0.0025f;
+        }
+        else if (changerPosition > 0.15f)
+        {
+            speedAcceleration = 0.003f;
+        }
+        else if (changerPosition > 0.10f)
+        {
+            speedAcceleration = 0.0035f;
+        }
+        else if (changerPosition > -0.10f)
+        {
+            speedAcceleration = 0.0035f;
+        }
+        else if (changerPosition > -0.15f)
+        {
+            speedAcceleration = 0.003f;
+        }
+        else if (changerPosition > -0.20f)
+        {
+            speedAcceleration = 0.0025f;
+        }
+        else if (changerPosition > -0.25f)
+        {
+            speedAcceleration = 0.0018f;
+        }
+        else if (changerPosition > -0.30f)
+        {
+            moveDirectory = "up";
+            speedAcceleration = 0.0008f;
+        }
+    }
+
+    private void changePosition()
+    {
+        if (moveDirectory == "up")
+        {
+            changerPosition += speedAcceleration * jumpingSpeed;
+        }
+        else
+        {
+            changerPosition -= speedAcceleration * jumpingSpeed;
+        }
+    }
+
+}
